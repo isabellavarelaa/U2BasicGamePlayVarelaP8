1d0299f578340938f40be1a91c31aed9f2a8ab79
isabellavarelaa <727144@student.dusd.net>
2025-03-26 15:19:43 -0700

    finish
    finish

--- BasicGamePlayVarelaP8/Assets/Scripts/SpawnManager.cs
+++ BasicGamePlayVarelaP8/Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,16 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    public float sideSpawnMinZ;
+    public float sideSpawnMaxZ;
+    public float sideSpawnX;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("SpawnRandomAnimal", 2, 1.5f);
     }
 
     // Update is called once per frame
